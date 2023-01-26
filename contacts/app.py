7892e74c4a5312286d9f2b5dd1d4f640796803e9
Sam Jones <sam@imnotpete.com>
2023-01-26 13:36:03 -0500

    working crud operations

--- contacts/app.py
+++ contacts/app.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ class Contact:
     email: str
     phone: str
 
+# data source
 contacts = {
     1: Contact(1, "John", "john email", "john phone"),
     2: Contact(2, "Sue", "Sue email", "Sue phone"),
@@ -17,25 +19,51 @@ contacts = {
 }
 
 def _find_next_id():
-    return max(contact.id for contact in contacts) + 1
+    return max(contacts.keys()) + 1
 
 @app.get("/contacts")
 def get_contacts():
-    return jsonify(contacts.values)
+    return jsonify(list(contacts.values()))
 
-@app.get("/contacts/<id>")
+@app.get("/contacts/<int:id>")
 def get_contact(id):
-    print(type(id))
-    return jsonify(contacts[int(id)])
+    if id in contacts:
+        return jsonify(contacts[id])
+
+    return "Contact not found", 404
 
 @app.post("/contacts")
 def create_contact():
     if request.is_json:
         contact = request.get_json()
+
+        if "id" in contact:
+            return "Cannot specify ID for new contact - did you mean to update with PUT?", 400
+
         id = _find_next_id()
-        contact["id"] = id
-        contact = Contact(**contact)
+
+        contact = Contact(id, **contact)
         contacts[id] = contact
+
         return {"id": id}, 201
     
     return "error", 500
+
+@app.put("/contacts/<int:id>")
+def update_contact(id):
+    if request.is_json:
+        contact = request.get_json()
+        contact = Contact(**contact)
+
+        if contact.id != id:
+            return "error", 500
+        
+        contacts[id] = contact
+        return {}, 201
+    
+    return "error", 500
+
+@app.delete("/contacts/<int:id>")
+def delete_contact(id):
+    contacts.pop(id)
+    return {}, 201
