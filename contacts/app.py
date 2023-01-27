c22543bada15a05193fd37f03ecf8aca7f75b371
Sam Jones <sam@imnotpete.com>
2023-01-27 15:05:58 -0500

    switched to postgres db with sqlalchemy; get-all working

--- contacts/app.py
+++ contacts/app.py
@@ -1,69 +1,78 @@
-from dataclasses import dataclass
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
-import json
+from dataclasses import dataclass
+import psycopg2
 
+
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432'
+db.init_app(app)
 
 @dataclass
-class Contact:
-    id: int
-    name: str
-    email: str
-    phone: str
+class Contact(db.Model):
+    __tablename__ = 'contacts'
 
-# data source
-contacts = {
-    1: Contact(1, "John", "john email", "john phone"),
-    2: Contact(2, "Sue", "Sue email", "Sue phone"),
-    3: Contact(3, "Steve", "Steve email", "Steve phone"),
-}
-
-def _find_next_id():
-    return max(contacts.keys()) + 1
+    id:int = db.Column(db.Integer, primary_key=True)
+    name:str = db.Column(db.String(40), nullable=False)
+    email:str = db.Column(db.String(40), nullable=False)
+    phone:str = db.Column(db.String(10), nullable=False)
 
 @app.get("/contacts")
 def get_contacts():
-    return jsonify(list(contacts.values()))
+    # contacts = Contact.query.all()
+    contacts = db.session.execute(db.select(Contact)).scalars()
+    # contacts = [contact for contact in contacts]
 
-@app.get("/contacts/<int:id>")
-def get_contact(id):
-    if id in contacts:
-        return jsonify(contacts[id])
+    if not contacts:
+        return "No contacts found", 404
 
-    return "Contact not found", 404
+    return jsonify(list(contacts))
 
-@app.post("/contacts")
-def create_contact():
-    if request.is_json:
-        contact = request.get_json()
+# @app.get("/contacts/<int:id>")
+# def get_contact(id):
+#     cur = conn.cursor()
+#     # cur.execute('SELECT * FROM contacts where id=%s', (id,))
+#     cur.execute('SELECT * FROM contacts where id=%(id)s', {"id": id})
+#     contact = cur.fetchone()
 
-        if "id" in contact:
-            return "Cannot specify ID for new contact - did you mean to update with PUT?", 400
+#     if contact:
+#         return jsonify(contact)
 
-        id = _find_next_id()
+#     return "Contact not found", 404
 
-        contact = Contact(id, **contact)
-        contacts[id] = contact
+# @app.post("/contacts")
+# def create_contact():
+#     if request.is_json:
+#         contact = request.get_json()
 
-        return {"id": id}, 201
+#         if "id" in contact:
+#             return "Cannot specify ID for new contact - did you mean to update with PUT?", 400
+
+#         id = _find_next_id()
+
+#         contact = Contact(id, **contact)
+#         contacts[id] = contact
+
+#         return {"id": id}, 201
     
-    return "error", 500
+#     return "error", 500
 
-@app.put("/contacts/<int:id>")
-def update_contact(id):
-    if request.is_json:
-        contact = request.get_json()
-        contact = Contact(**contact)
+# @app.put("/contacts/<int:id>")
+# def update_contact(id):
+#     if request.is_json:
+#         contact = request.get_json()
+#         contact = Contact(**contact)
 
-        if contact.id != id:
-            return "error", 500
+#         if contact.id != id:
+#             return "error", 500
         
-        contacts[id] = contact
-        return {}, 201
+#         contacts[id] = contact
+#         return {}, 201
     
-    return "error", 500
+#     return "error", 500
 
-@app.delete("/contacts/<int:id>")
-def delete_contact(id):
-    contacts.pop(id)
-    return {}, 201
+# @app.delete("/contacts/<int:id>")
+# def delete_contact(id):
+#     contacts.pop(id)
+#     return {}, 201
