69492908d823d11929d8ef5bd3dca1a537f0fa1f
Sam Jones <sam@imnotpete.com>
2023-01-27 15:30:18 -0500

    get one, create, update all working on db

--- contacts/app.py
+++ contacts/app.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, jsonify
 from dataclasses import dataclass
-import psycopg2
 
 
 db = SQLAlchemy()
@@ -13,64 +12,62 @@ db.init_app(app)
 class Contact(db.Model):
     __tablename__ = 'contacts'
 
-    id:int = db.Column(db.Integer, primary_key=True)
+    id:int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name:str = db.Column(db.String(40), nullable=False)
     email:str = db.Column(db.String(40), nullable=False)
     phone:str = db.Column(db.String(10), nullable=False)
 
+# for testing - create blank database with our table(s)
+with app.app_context():
+    db.create_all()
+
 @app.get("/contacts")
 def get_contacts():
-    # contacts = Contact.query.all()
     contacts = db.session.execute(db.select(Contact)).scalars()
-    # contacts = [contact for contact in contacts]
 
     if not contacts:
         return "No contacts found", 404
 
     return jsonify(list(contacts))
 
-# @app.get("/contacts/<int:id>")
-# def get_contact(id):
-#     cur = conn.cursor()
-#     # cur.execute('SELECT * FROM contacts where id=%s', (id,))
-#     cur.execute('SELECT * FROM contacts where id=%(id)s', {"id": id})
-#     contact = cur.fetchone()
+@app.get("/contacts/<int:id>")
+def get_contact(id):
+    contact = db.session.execute(db.select(Contact).where(Contact.id==id)).scalar()
 
-#     if contact:
-#         return jsonify(contact)
+    if not contact:
+        return "Contact not found", 404
 
-#     return "Contact not found", 404
+    return jsonify(contact)
 
-# @app.post("/contacts")
-# def create_contact():
-#     if request.is_json:
-#         contact = request.get_json()
+@app.post("/contacts")
+def create_contact():
+    if request.is_json:
+        contact = request.get_json()
 
-#         if "id" in contact:
-#             return "Cannot specify ID for new contact - did you mean to update with PUT?", 400
+        if "id" in contact:
+            return "Cannot specify ID for new contact - did you mean to update with PUT?", 400
 
-#         id = _find_next_id()
+        contact = db.session.execute(db.insert(Contact).returning(Contact), contact).scalar()
+        db.session.commit()
 
-#         contact = Contact(id, **contact)
-#         contacts[id] = contact
-
-#         return {"id": id}, 201
+        return {"id": contact.id}, 201
     
-#     return "error", 500
+    return "error", 500
 
-# @app.put("/contacts/<int:id>")
-# def update_contact(id):
-#     if request.is_json:
-#         contact = request.get_json()
-#         contact = Contact(**contact)
+@app.put("/contacts/<int:id>")
+def update_contact(id):
+    if request.is_json:
+        contact = request.get_json()
 
-#         if contact.id != id:
-#             return "error", 500
+        if contact["id"] != id:
+            return "ID in URL and body do not match", 500
         
-#         contacts[id] = contact
-#         return {}, 201
+        db.session.execute(db.update(Contact), contact)
+        db.session.commit()
+
+        return {}, 201
     
-#     return "error", 500
+    return "error", 500
 
 # @app.delete("/contacts/<int:id>")
 # def delete_contact(id):
